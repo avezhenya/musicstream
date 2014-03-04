a1e09d1958691eee9c9f0a226b15241a99080f7d
avezhenya <ave-zhenya@yandex.ru>
2014-03-05 00:53:01 +0900

    All project

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python
+#-*- coding: utf-8 -*-
+import os.path
+from flask import Flask
+
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+from app import views
